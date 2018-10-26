84010886aa8f03de9aa5140449369101ef7ee747
Hulsbosch <j.hulsbosch@colours.nl>
2018-10-26 21:49:02 +0200

    disable csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,9 +29,9 @@ module.exports = {
     },
     {
       resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+      // options: {
+      //   modulePath: `${__dirname}/src/cms/cms.js`,
+      // },
     },
     'gatsby-plugin-eslint',
     {
