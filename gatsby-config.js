5ef1297e04bc12a1e74f985c05fb9483915a67e8
Joris Hulsbosch <joris.hulsbosch@colours.nl>
2018-11-27 22:22:00 +0100

    set a feature policy

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
             'X-XSS-Protection: 1; mode=block',
             'X-Content-Type-Options: nosniff',
             'Referrer-Policy: no-referrer-when-downgrade',
-            'Feature-Policy: * \'self\'',
+            'Feature-Policy: autoplay \'self\'',
             'Content-Security-Policy: default-src \'self\'; script-src \'self\' \'unsafe-inline\' https://portfolioris.nl; style-src \'self\' \'unsafe-inline\'; img-src \'self\' data: https://res.cloudinary.com https://portfolioris.nl',
           ],
         }, // option to add more headers. `Link` headers are transformed by the below criteria
