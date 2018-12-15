9a8575546d34af7a611f431e77c75bf53cfb2f71
Joris Hulsbosch <joris.hulsbosch@colours.nl>
2018-12-15 15:39:10 +0100

    extend hide utilities

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -46,7 +46,7 @@ const Header = ({
                 </div>
               </div>
             </div>
-            <div className="o-layout__fit  u-hidden@desk">
+            <div className="o-layout__fit  u-hidden@desktop">
               <MenuToggle
                 handleToggleMenu={handleToggleMenu}
                 isOpen={menuIsOpen}
